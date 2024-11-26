3cab6111f3917b08dbda772c6b7083c9abb6fcc2
Yasu1025 <sillyjp25@gmail.com>
2024-11-26 14:43:54 +0900

    [Feat] setup models

--- backend/app/Models/Coupon.php
+++ backend/app/Models/Coupon.php
@@ -2,9 +2,30 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Coupon extends Model
 {
-    //
+  use HasFactory;
+
+  protected $fillable = [
+    'name',
+    'discount',
+    'valid_until'
+  ];
+
+  // convert name to Uppsercase
+  public function setNameAttribute($value)
+  {
+    $this->attributes['name'] = Str::upper($value);
+  }
+
+  // Check coupon is valid
+  public function checkIfValid()
+  {
+    return ($this->valid_until > Carbon::now()) ? true : false;
+  }
 }
--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -9,40 +9,63 @@ use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+  /** @use HasFactory<\Database\Factories\UserFactory> */
+  use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array<int, string>
+   */
+  protected $fillable = [
+    'name',
+    'email',
+    'password',
+    'address',
+    'city',
+    'zip_code',
+    'country',
+    'phone_number',
+    'profile_image',
+    'profile_completed'
+  ];
+
+  /**
+   * The attributes that should be hidden for serialization.
+   *
+   * @var array<int, string>
+   */
+  protected $hidden = [
+    'password',
+    'remember_token',
+  ];
+
+  protected $appends = [
+    'image_path'
+  ];
+
+  /**
+   * Get the attributes that should be cast.
+   *
+   * @return array<string, string>
+   */
+  protected function casts(): array
+  {
+    return [
+      'email_verified_at' => 'datetime',
+      'password' => 'hashed',
     ];
+  }
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+  public function orders()
+  {
+    return $this->hasMany(Order::class)->with('products')->latest();
+  }
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
+  public function getImagePathAttribute()
+  {
+    return $this->profile_image
+      ? asset($this->profile_image)
+      : 'https://cdn.pixabay.com/photo/2017/11/10/05/48/user-2935527_1280.png';
+  }
 }
