1bc5a6bb7598a056223aa3217e813df4a7bc77df
Yasu1025 <sillyjp25@gmail.com>
2024-11-29 16:17:33 +0900

    [FE] load more

--- frontend/src/components/products/ProductList.tsx
+++ frontend/src/components/products/ProductList.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { IProduct } from "../../types";
 import ProductItem from "./ProductItem";
 
@@ -6,15 +7,30 @@ interface Props {
 }
 
 const ProductList = ({ products }: Props): JSX.Element => {
+  const [productsToShow, setProductsToShow] = useState(5);
+
+  const loadMoreProducts = () => {
+    if (productsToShow > products.length) return;
+    setProductsToShow((prev) => prev + 5);
+  };
+
   return (
-    <div className="flex flex-wrap -m-4">
-      {products.length ? (
-        products.map((product) => (
+    <div className="-m-4">
+      <div className="flex flex-wrap ">
+        {products.slice(0, productsToShow).map((product) => (
           <ProductItem key={product.id} product={product} />
-        ))
-      ) : (
-        <div>No products......</div>
-      )}
+        ))}
+      </div>
+      <div className="flex justify-center mt-2">
+        {productsToShow < products.length && (
+          <button
+            onClick={loadMoreProducts}
+            className="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+          >
+            LoadMore
+          </button>
+        )}
+      </div>
     </div>
   );
 };
