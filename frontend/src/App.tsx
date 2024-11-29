0004d727bfd54671668306c72c4f3c7c45fbc532
Yasu1025 <sillyjp25@gmail.com>
2024-11-29 20:22:24 +0900

    [FE] fix routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,19 @@
-import { RouterProvider } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import routes from "./routes";
 import Header from "./components/layouts/Header";
 
 const App = (): JSX.Element => {
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <div className="container mx-auto py-8 px-4">
-        <RouterProvider router={routes} />
+        <Routes>
+          {routes.map((r) => (
+            <Route key={r.path} path={r.path} element={r.element} />
+          ))}
+        </Routes>
       </div>
-    </>
+    </BrowserRouter>
   );
 };
 
